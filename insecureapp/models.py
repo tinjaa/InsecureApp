0ca9023bb2dc8e663fb6deaf6b860ca1bef72ce5
tisormun <tinja.i.sormunen@student.jyu.fi>
2024-11-28 16:47:24 +0200

    Actual final commit, with comments for fixes

--- insecureapp/models.py
+++ insecureapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# 5. Flaw Cryptographic Failure
+
 class User(models.Model):
     username = models.CharField(max_length=100)
+    # 5. Flaw Cryptographic Failure
     password = models.CharField(max_length=100)
+    #admin = models.BooleanField(default=False)
--- insecureapp/views.py
+++ insecureapp/views.py
@@ -2,36 +2,61 @@ from django.shortcuts import render
 from django.db import connection
 from django.http import HttpResponse
 from .models import User
+#import logging
+#logger = logging.getLogger(__name__)
+#from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.password_validation import validate_password
+#from django.core.exceptions import ValidationError
+#from django.contrib.auth.models import User
+#from django.contrib.auth.hashers import make_password
 
-# 1. Flaw SQL Injection 
+
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         cursor = connection.cursor()
+
+        # 1. Flaw SQL Injection 
         response = cursor.execute(f"SELECT * FROM insecureapp_user WHERE username = '{username}' AND password = '{password}'")
+        # response = cursor.execute("SELECT * FROM insecureapp_user WHERE username = %s AND password = %s", [username, password])
+        # response = User.objects.filter(username=username, password=password).first()
 
         if response:
             return HttpResponse("Login successful!")
         else:
             # 2. Flaw Security Logging and Monitoring Failure
+            # logger.warning(f"Failed login attempt for username: {username}")
             return HttpResponse("Invalid credentials!")
     
     return render(request, 'login.html')
 
 # 3. Flaw Broken Access Control
+#@login_required
 def admin(request):
+    #if not request.user.admin:
+    #    return HttpResponse("Access Denied", status=403)
     return HttpResponse("Hello everybody! This view should be available only for admins.")
 
-# 4. Flaw Identification and Authentication Failure
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
+        #hashedpassword = make_password(password)
+        #User.objects.create(username=username,password=password)
+
+        # 4. Flaw Identification and Authentication Failure
         User.objects.create(username=username, password=password)
-        return HttpResponse("User created!")
+        #return HttpResponse("User created!")
+
+        #try:
+        #    validate_password(password)
+        #    User.objects.create(username=username, password=password)
+        #    return HttpResponse("User created!")
+        #except ValidationError as e:
+        #    return HttpResponse(f"Too weak password: {e}", status=400)
 
     return render(request, 'signup.html')
 
